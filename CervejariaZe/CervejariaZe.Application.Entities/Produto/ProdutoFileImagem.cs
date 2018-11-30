1020d1cc5bcc9d9018c97d65ad9f82b6b75900b8
Vinicius Santana <vinicius.santanna@hotmail.com>
2018-11-30 10:15:17 -0300

    Commit diario

---
+++ CervejariaZe/CervejariaZe.Application.Entities/Produto/ProdutoFileImagem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CervejariaZe.Application.Entities.Produto
+{
+    public class ProdutoFileImagem
+    {
+        public string name { get; set; }
+        public byte[] stream { get; set; }
+        public int tamanho { get; set; }
+    }
+}
---
+++ CervejariaZe/CervejariaZe.Domain.Entities/Contantes/Mensagens.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CervejariaZe.Domain.Entities
+{
+    public class Mensagens
+    {
+        public const string MensagemCadastroSucesso = "Registro Salvo com Sucesso!";
+        public const string RegistroExistente = "Registro já existe!";
+    }
+}
